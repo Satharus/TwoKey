1d4935e8c4420a6849efde09790f1f8f744f73b7
Satharus <a.elmayyah@gmail.com>
2021-04-09 20:03:49 +0200

    Added login checking.

--- Browser extension/popup.js
+++ Browser extension/popup.js
@@ -1,42 +1,34 @@
 (function()
 {
-//    var btn=document.getElementById('LoginBtn');
-//    btn.addEventListener('click',openapp);
-//    function openapp()
-//    {
-//     var port = chrome.runtime.connectNative('com.tk.twokey');
-//     port.postMessage("hello");
-// //     //console.log(activeUrl);
-// //     let message='hello';
-// //     port.onMessage.addListener(messagesent )
-// //     function messagesent(){console.log("Received"+message );}
-// //     port.postMessage(messages)
-// //     port.onDisconnect.addListener(function(error) {
-// //             console.log(error)
-// //             console.log("last error:" + chrome.runtime.lastError.message)
-// //     });
-//    }
-var btn1=document.createElement("BUTTON");
-btn1.innerHTML="Fill";
-var x = document.createElement("P");
-x.innerText="Welcome again";
+	var btn1=document.createElement("BUTTON");
+	btn1.innerHTML="Fill";
+	var x = document.createElement("P");
+	x.innerText="Welcome again";
 
-var btn=document.getElementById('LoginBtn');
-btn.addEventListener('click',f);
-function f()
-{
-        var masterpasswd=document.getElementById('masterpasswd').value;
-        var http=new XMLHttpRequest();
-        const url="http://localhost:8000/?masterpasswd="+masterpasswd+"&email=newuser@gmail.com";
-        http.open("GET",url,true);
-        http.send();
-        http.onreadystatechange = (e) => {
-        console.log(http.responseText);
-  }
-    document.body.innerHTML="Two Key";
-    document.body.appendChild(x);
-    document.body.appendChild(btn1);
-    
-}
+	var btn=document.getElementById('LoginBtn');
+	btn.addEventListener('click',f);
+	function f()
+	{
+			var masterpasswd=document.getElementById('masterpasswd').value;
+			var email=document.getElementById('email').value;
+			var http=new XMLHttpRequest();
+			const url="http://localhost:8000/?masterpasswd="+masterpasswd+"&email=" + email;
+			http.open("GET",url,true);
+			http.send();
+			http.onreadystatechange = (e) => {
+			console.log(http.readyState);
+			if (http.readyState == 4 && http.status == 200) {
+				document.body.innerHTML="Two Key";
+				document.body.appendChild(x);
+				document.body.appendChild(btn1);
+			}
+			else if (http.readyState == 4)
+			{
+				alert("wrong password");
+			}
+			console.log(http.responseText);
+		  }
+
+	}
 
 })();
